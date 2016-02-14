1c1704ddc84ae6adc65ebfefc56f2facdfab7586
rene-ye <rene.ye@hotmail.com>
2016-02-14 00:30:06 -0800

    Added Controllers
    Added MY_Controller and created the application class.
    Added basic template.
    Added the starting point for every page.

---
+++ application/views/login.php
@@ -0,0 +1,18 @@
+<div id="contents">
+    <div id="tagline" class="clearfix">
+        <h1>This is the Login page</h1>
+        <div>
+                <p>
+                        Please enter your username and password.
+                </p>
+                <form>
+                <fieldset>
+                 <legend>Login:</legend>
+                 Username: <input type="text"><br>
+                 Password: <input type="text"><br>
+                </fieldset>
+               </form>
+               <button type="button" onclick="alert('Hello world!')">Log In</button>
+        </div>
+    </div>
+</div>
