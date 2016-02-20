f56df4cb1b25b0940f0eed03695e6efd308a9a30
ScrawnySquirrel <s.hyoung.238@gmail.com>
2016-02-19 16:15:08 -0800

    History page implementation by Vivek

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,7 +50,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'welcome';
-$route['stocks/(:any)'] = 'history/index/$1';
+$route['stocks/(:any)'] = 'history/displayStock/$1';
 $route['Players/(:any)'] = 'portfolio/index/$1';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
