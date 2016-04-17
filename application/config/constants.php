c3d15d7a16097d82242341039dbba0a80fea34ed
Joseph Tam <josephtam3@hotmail.com>
2016-04-17 09:15:01 -0700

    wrote functions to retrieve data from the server, register agents, buy/sell stocks, created the bsx_tokens and bsx_transactions models and the function to modify the tables

--- application/config/constants.php
+++ application/config/constants.php
@@ -83,3 +83,17 @@ defined('EXIT_USER_INPUT')     OR define('EXIT_USER_INPUT', 7); // invalid user 
 defined('EXIT_DATABASE')       OR define('EXIT_DATABASE', 8); // database error
 defined('EXIT__AUTO_MIN')      OR define('EXIT__AUTO_MIN', 9); // lowest automatically-assigned error code
 defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
+
+/*
+ * Custom defines 
+ */
+//define('BSX', 'http://www.comp4711bsx.local/');
+define('BSX', 'http://bsx.jlparry.com/');
+define('GAME_CLOSED', 0);
+define('GAME_SETUP', 1);
+define('GAME_READY', 2);
+define('GAME_OPEN', 3);
+define('GAME_OVER', 4);
+define('TEAM_CODE', 'O10');
+define('TEAM_NAME', 'pink-ranger');
+define('TEAM_PW', 'tuesday');
